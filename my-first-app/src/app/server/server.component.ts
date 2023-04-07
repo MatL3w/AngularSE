430baa03df85bd14946e0815badccfba11cf38b8
Lecislav <lecislav@protonmail.com>
2023-04-07 21:36:28 +0200

    directives

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,16 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [
-    `
-      h3 {
-        color: blue;
-      }
-      .smooth-transition {
-        transition: all 0.2s ease-in-out;
-      }
-    `,
-  ],
+
 })
 export class ServerComponent {
   width= 200;
@@ -21,14 +12,17 @@ export class ServerComponent {
   innertextExample = 'text inserted in elements';
   serverStatus: string = 'offline';
   condition = false;
-
+  color = 'FFFFFF';
   constructor() {
+
     setInterval(() => {
       if (this.condition) {
         this.condition = false;
       } else {
         this.condition = true;
       }
+      this.color=this.getRandomColor();
+      console.log('lol');
     }, 3000);
   }
   onClickButton(){
@@ -37,5 +31,25 @@ export class ServerComponent {
       } else {
         this.condition = true;
       }
+  }
+  getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+  }
+  async fetchData(){
+        await fetch('https://localhost:3000/signin', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: 'mati@vp.pl',
+            password: 'strongPassword',
+          }),
+        }).then(res=>console.log(res));
   }
 }
