ef0877dac0602594009abbe75c16600c038b1245
Lecislav <lecislav@protonmail.com>
2023-05-24 16:13:58 +0200

    *

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ServerComponent } from './server/server.component';
 import { LinkComponent } from './link/link.component';
 import { SubElement1Component } from './sub-element1/sub-element1.component';
 import { SubElement2Component } from './sub-element2/sub-element2.component';
-import {Basic} from './server/basic.directive'
+import { Basic } from './server/basic.directive'
 import { Structural } from './server/structural.directive';
 import { LoggingService } from './logging.service';
 import { ServiceToInject } from './serviceToInject.service';
