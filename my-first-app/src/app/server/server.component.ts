531d6cec3018d275de5c398c0d0cecc9be14ab66
Lecislav <lecislav@protonmail.com>
2023-05-26 13:55:54 +0200

    *

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -84,5 +84,6 @@ export class ServerComponent implements OnInit {
   }
   subjectEmit(){
     this.loggingService.subjectEmitter.emit(Math.random());
+    this.loggingService.logMessage('subjectEmit');
   }
 }
