76cf31554983a176e8062d28e3e6119f90699b8e
Lecislav <lecislav@protonmail.com>
2023-06-06 18:39:42 +0200

    *

--- my-first-app/src/app/server/uppercase.pipe.ts
+++ my-first-app/src/app/server/uppercase.pipe.ts
@@ -2,6 +2,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
   name: 'Uppercase',
+  pure: false,
 })
 export class Uppercase implements PipeTransform {
   transform(value: string, ...args: any[]) {
